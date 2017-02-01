7c55c3cdcc7ae7ddb707cef05dfb3166644dae6d
Gaurav Agarwal <sales@appperfect.com>
2017-02-01 12:37:23 +0530

    Fix path issues in scripts
    Former-commit-id: 826d67ae90532865339753a2e81bb0a6704706b0

--- queryio-parent/service/bin/start_agent.sh
+++ queryio-parent/service/bin/start_agent.sh
@@ -1,3 +1,3 @@
-cd '$USER_PACKAGE_INSTALL_DIR$/QueryIOAgent/bin'
+cd "$USER_PACKAGE_INSTALL_DIR$/QueryIOAgent/bin"
 echo 'Starting QueryIO agent'
 sh startQIOAgent.sh $USER_PACKAGE_INSTALL_DIR$/QueryIOAgent 6680
