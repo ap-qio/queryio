afd442feed558d036e3f84b358a7da2832b66e47
sagarappperfect <sagar@appperfect.com>
2021-12-17 16:34:29 +0530

    - Upgraded Hive from 2.1.1 to 3.1.2 version

--- queryio-parent/service/hive-2.1.1/bin/startHive.sh
+++
@@ -1,6 +0,0 @@
-bin=`dirname "$0"`
-bin=`cd "$bin"; pwd`
-
-echo $bin
-mkdir -p $bin/../logs/
-nohup $bin/hive --service hiveserver2 -hiveconf hive.root.logger=DEBUG,console >> $bin/../logs/out.log 2>&1 &
